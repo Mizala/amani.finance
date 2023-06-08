2b36dcd18ccbc77055219d9a94f8c10ae9649c48
ifedeejoy <ifedeejoy@gmail.com>
2023-06-08 11:33:11 +0100

    updated env and index

--- src/index.ts
+++ src/index.ts
@@ -44,6 +44,6 @@ app.use('/user', userRoutes);
 app.use('/auth', authRoutes);
 app.use('/expense', expenseRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
