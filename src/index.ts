d75bffbcdeb6100f1d5ef471b140f7c7f03dc8dc
Micheal Akinwonmi <micheal.akinwonmi@jazzhr.com>
2023-06-16 06:50:41 -0600

    Resolved redis issue

--- src/index.ts
+++ src/index.ts
@@ -4,31 +4,35 @@ dotenv.config({});
 
 import express from 'express';
 import mongoose from 'mongoose';
+import logger from "morgan";
+
 import cors from 'cors';
 import userRoutes from './domain/user/routes/UserRoutes';
 import authRoutes from './application/auth/AuthRoutes';
 import expenseRoutes from './domain/expense/routes/ExpenseRoutes';
 import { createBullBoard } from '@bull-board/api';
 import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
-// import { ExpressAdapter } from '@bull-board/express';
+import { ExpressAdapter } from '@bull-board/express';
 // import { router } from 'bull-board'
-// import {analysisWorker, analysisQueue} from './services/queueService';
+import {analysisWorker, analysisQueue} from './services/queueService';
 
 
 const app = express();
 
 // setup queue server & board
-// const serverAdapter = new ExpressAdapter();
-// serverAdapter.setBasePath('/admin/queues');
+const serverAdapter = new ExpressAdapter();
+serverAdapter.setBasePath('/admin/queues');
 
-// const { addQueue, removeQueue, setQueues, replaceQueues } = createBullBoard({
-//   queues: [new BullMQAdapter(analysisQueue)],
-//   serverAdapter: serverAdapter,
-// });
+const { addQueue, removeQueue, setQueues, replaceQueues } = createBullBoard({
+  queues: [new BullMQAdapter(analysisQueue)],
+  serverAdapter: serverAdapter,
+});
 // end queue server & board setup
 
 app.use(cors());
 app.use(express.json());
+app.use(logger("dev"));
+
 // app.use('/admin/queues', serverAdapter.getRouter());
 
 // Validate that JWT_SECRET is set
@@ -44,6 +48,7 @@ mongoose.connect(`${process.env.MONGODB_URL}/amani`)
 app.use('/user', userRoutes);
 app.use('/auth', authRoutes);
 app.use('/expense', expenseRoutes);
+
 
 const PORT = process.env.PORT || 3000;
 
