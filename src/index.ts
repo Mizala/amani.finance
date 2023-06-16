528faec529127427a8ee47af430d11e3827dc843
Micheal Akinwonmi <micheal.akinwonmi@jazzhr.com>
2023-06-16 06:42:37 -0600

    Resolved redis issue

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 // src/index.ts
+import dotenv from  "dotenv";
+dotenv.config({});
 
 import express from 'express';
 import mongoose from 'mongoose';
@@ -6,30 +8,28 @@ import cors from 'cors';
 import userRoutes from './domain/user/routes/UserRoutes';
 import authRoutes from './application/auth/AuthRoutes';
 import expenseRoutes from './domain/expense/routes/ExpenseRoutes';
-import dotenv from  "dotenv";
 import { createBullBoard } from '@bull-board/api';
 import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
-import { ExpressAdapter } from '@bull-board/express';
+// import { ExpressAdapter } from '@bull-board/express';
 // import { router } from 'bull-board'
-import {analysisWorker, analysisQueue} from './services/queueService';
+// import {analysisWorker, analysisQueue} from './services/queueService';
 
-dotenv.config({});
 
 const app = express();
 
 // setup queue server & board
-const serverAdapter = new ExpressAdapter();
-serverAdapter.setBasePath('/admin/queues');
+// const serverAdapter = new ExpressAdapter();
+// serverAdapter.setBasePath('/admin/queues');
 
-const { addQueue, removeQueue, setQueues, replaceQueues } = createBullBoard({
-  queues: [new BullMQAdapter(analysisQueue)],
-  serverAdapter: serverAdapter,
-});
+// const { addQueue, removeQueue, setQueues, replaceQueues } = createBullBoard({
+//   queues: [new BullMQAdapter(analysisQueue)],
+//   serverAdapter: serverAdapter,
+// });
 // end queue server & board setup
 
 app.use(cors());
 app.use(express.json());
-app.use('/admin/queues', serverAdapter.getRouter());
+// app.use('/admin/queues', serverAdapter.getRouter());
 
 // Validate that JWT_SECRET is set
 const secret = process.env.JWT_SECRET;
@@ -37,7 +37,7 @@ if (!secret) {
   throw new Error('JWT_SECRET is not set in the environment variables.');
 }
 
-mongoose.connect(`${process.env.MONDOGB_URL}/amani`)
+mongoose.connect(`${process.env.MONGODB_URL}/amani`)
 .then(() => console.log('MongoDB Connected...'))
 .catch((err) => console.log(err));
 
--- src/services/queueService.ts
+++ src/services/queueService.ts
@@ -4,16 +4,33 @@ import { Queue, Worker } from 'bullmq';
 // import {BullAdapter} from '@bull-board/api/bullAdapter';
 import ExpenseService from '../domain/expense/services/ExpenseService';
 import IORedis from 'ioredis';
-const redisConfig: any = {
-  port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379,
-  host: process.env.REDIS_HOST || 'localhost',
-};
+let connection: any = null;
 
-if(process.env.REDIS_PASSWORD){
-  redisConfig.password = process.env.REDIS_PASSWORD;
+if(process.env.REDIS_URL){
+  connection = new IORedis(process.env.REDIS_URL);
+}else{
+
+  const redisConfig: any = {
+    port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379,
+    host: process.env.REDIS_HOST || 'localhost',
+  };
+
+  if(process.env.REDIS_PASSWORD){
+    redisConfig.password = process.env.REDIS_PASSWORD;
+  }
+  connection = new IORedis(redisConfig);
 }
 
-const connection = new IORedis(redisConfig);
+connection.on("connected", function () {
+  console.log("Redis is connected");
+});
+
+connection.on("error", function (err: any) {
+  console.log("Redis error.", err);
+  console.log(err.message, err, {}, true);
+});
+
+
 
 // Create an analysis queue
 export const analysisQueue = new Queue('analysis', {connection});
@@ -29,7 +46,7 @@ export const analysisWorker = new Worker('analysis', async job => {
   } catch (err) {
     console.error(err);
   }
-});
+},  {connection});
 
 analysisWorker.on('completed', (job) => {
   console.log(`Job completed with ID ${job.id}`);
