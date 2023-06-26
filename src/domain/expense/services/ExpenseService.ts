826d7f9a88fc46e476e4dbd8fa84b6794ddb9aee
ifedeejoy <ifedeejoy@gmail.com>
2023-06-26 13:13:02 +0100

    updated emai bodyl and sent from

--- src/domain/expense/services/ExpenseService.ts
+++ src/domain/expense/services/ExpenseService.ts
@@ -77,15 +77,15 @@ class ExpenseService {
       const resultUrl = process.env.FRONT_END_URL + '/expense/' + expense;
       const sendEmail = await axios.post(`${emailUrl}/v1/emails/template`, {
         "template": "default",
-        "subject": "Your ChatGPT Expense Analysis Result Is Here",
-        "from": "noreply@amani.finance",
+        "subject": "Your Monthly Income Analysis Is Here",
+        "from": "no-reply@mizala.co",
         "recipients": [
           email
         ],
         "data": {
           "salutation": "Hey there 🎉,",
           "message": [    
-            `<p style="font-size: 16px;">Your bank statement has been successfully analyzed by ChatGPT. The insights from your statement are now ready for you to view.</p> <br/>`,
+            `<p style="font-size: 16px;">Your bank statement has been successfully analyzed by Mizala. The insights from your statement are now ready for you to view.</p> <br/>`,
             `<p style="font-size: 16px;">Please click the button below to view the results:</p> <br/>`,
             `<div style="display: flex; margin:auto; justify-content: center;"><a href="${resultUrl}" style="display: inline-block; padding: 10px 20px; font-size: 20px; cursor: pointer; text-align: center; text-decoration: none; outline: none; color: #fff; background-color: #322074; border: none; border-radius: 10px; box-shadow: 0 5px #999;">View Results</a></div> <br/>`,
             `<p style="font-size: 16px;">you can copy the link to access it directly: ${resultUrl}</p> <br/>`,
